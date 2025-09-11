1744e0c11c51bc3c977de04f1583f3cd7dbc6359
IsabelVargas2 <mvargas859@unab.edu.co>
2025-09-11 12:43:15 -0500

    layouts basicos

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
+    @extends('layout.app')
+
+
+    @section('content')
+
+    
     <h1>FORMULARIO PARA CREAR UN NUEVO PRODUCTO</h1>
 
     <form action="" method="post">
@@ -25,6 +23,4 @@
         <label for="brand">Brand:</label>
         <input type="text" name="brand">
     </form>
-
-</body>
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 });
 
 
-Route::profix('products')->controller(ProductController::class)->group(function (){
+Route::prefix('products')->controller(ProductController::class)->group(function (){
     Route::get('/', 'index' );
     Route::get('/create', 'create');
     Route::get('/{id}/{category?}', 'show');
