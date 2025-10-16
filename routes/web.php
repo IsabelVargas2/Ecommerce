a11aa8dcea4e896676515767c35a99ef09df6fd0
IsabelVargas2 <mvargas859@unab.edu.co>
2025-10-16 12:15:06 -0500

    Se configuro el login

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::prefix('products')->controller(ProductController::class)->group(function (){
@@ -14,3 +11,8 @@ Route::prefix('products')->controller(ProductController::class)->group(function 
     Route::get('/{id}/{category?}', 'show');
 });
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'welcome'] );
