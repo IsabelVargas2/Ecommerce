3e0a7f11bc13e0cb3fba567eac786d3959860eb7
IsabelVargas2 <mvargas859@unab.edu.co>
2025-10-16 11:32:47 -0500

    Ajuste para preparar el proyecto para laravel ui auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@yield('tittle')</title>
+    <!-- ✅ Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    
 
     @yield('css')
     
@@ -16,7 +19,8 @@
     @include('layouts.footer')
 
 
-
-
+    <!-- ✅ Bootstrap CSS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"></script> 
+ 
 </body>
 </html>
